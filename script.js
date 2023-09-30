5cc8222d0ba4c7f7dc7277e29d7577f639b880d1
BarryManning <102195741+BarryManning@users.noreply.github.com>
2023-09-29 22:58:03 -0400

    added coloring feature
    - adjusted canvas to be center aligned
    - added feature to color in pixels on hover

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const grid16 = document.getElementById('grid16');
 const container = document.getElementById('container');
 const pixel = document.getElementsByClassName('pixel');
+const hoveredDiv = document 
+let currentColor = ('black');
 
 grid16.onclick = function () {
-    for (let i = 0; i < 256; i++) {
-        const addPixel = document.createElement('div');
-        container.appendChild(addPixel)
-        addPixel.className = 'pixel'
+    if (container.childElementCount == 256) {
+        return
+    } else {
+        for (let i = 0; i < 256; i++) {
+            const addPixel = document.createElement('div');
+            container.appendChild(addPixel)
+            addPixel.className = 'pixel'
+        }
     }
 };
 
+function makeCurrentColor () {
+    pixel.style.color = currentColor;
+};
 
-
+window.addEventListener("mouseover", function (event) {
+    if (event.target.className == 'pixel') {
+        event.target.style.backgroundColor = 'black';
+        console.log('registering');
+    } 
+});
